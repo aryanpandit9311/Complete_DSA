2023214418a95b7f60fc69a0780c789f7ba12ddd
aryanpandit9311 <aryanpandit9311@gmail.com>
2026-08-11 10:53:07 +0530

    Time: 15 ms (91.24%), Space: 69.9 MB (61.73%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
@@ -8,37 +8,43 @@
  * }
  */
 class Solution {
-    TreeNode ans= null;
+    TreeNode ans = null;
 
-    public int fun(TreeNode root, TreeNode p, TreeNode q){
-int l=0;
-int r=0;
+    
 
-              if(root.left!=null){
-                    l= fun(root.left,p,q);
-              }
-               if(root.right!=null){
-                  r= fun(root.right,p,q);
-               }
-           
-               int self = 0;
-               if(root==p || root==q){
-                 self=1;
-                    }
-                    int total = l+r+self;
-                    if(total ==2 && ans==null){
-                        ans=root;
-                    }
+    public int fun(TreeNode root, TreeNode p, TreeNode q) {
+        int total = 0;
+        int self = 0;
+        int l = 0;
+        int r = 0;
+        if (root == null) {
+            return total;
+        }
 
-                    return total;
+       l= fun(root.left, p, q);
+      r=  fun(root.right, p, q);
+
+        if (root.val == q.val || root.val == p.val) {
+            self = 1;
+        }
+
+        total = l + r + self;
+        if (total == 2 && ans == null) {
+            ans = root;
+
+        }
+        return total;
+
     }
+
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        if(root==null){
+
+        if (root == null) {
             return root;
         }
-        fun(root,p,q);
-     
-       return ans;
 
+        fun(root, p, q);
+
+        return ans;
     }
 }
