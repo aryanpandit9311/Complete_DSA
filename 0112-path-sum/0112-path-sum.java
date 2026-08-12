f2662783d027381c418c7e9087f474ce26d80473
aryanpandit9311 <aryanpandit9311@gmail.com>
2026-08-12 16:07:38 +0530

    Time: 0 ms (100%), Space: 44.8 MB (68.2%) - LeetHub

--- 0112-path-sum/0112-path-sum.java
+++ 0112-path-sum/0112-path-sum.java
@@ -15,18 +15,13 @@
  */
 
 class Solution {
-
     public boolean hasPathSum(TreeNode root, int targetSum) {
         if(root==null){
             return false;
         }
-
-           // Check whether current node is a leaf
         if (root.left == null && root.right == null) {
             return root.val == targetSum;
         }
-
-        // Continue searching in left or right subtree
         return hasPathSum(root.left, targetSum - root.val)
             || hasPathSum(root.right, targetSum - root.val);
     }
